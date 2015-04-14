3db8ce6cec312e983bd2992e3e25fc0aeddb13e8
Nick Brandon <nbrandon@incomm.com>
2015-04-14 10:12:25 -0700

    initial commit

---
+++ wp-content/themes/textback/footer.php
@@ -0,0 +1,16 @@
+<div class="clearer">&nbsp;</div>
+<div align="center">
+  <table>
+    <tr>
+      <td><ul id="yearpaginate">
+          <li><h6><a href="mailto:happyhairdo@gmail.com" <?php if ( is_page(contact) ) { ?>class="hi"<?php } ?>>email</a></h6></li>
+	  </ul></td>
+    </tr>
+  </table>
+</div>
+<div class="clearer">&nbsp;</div>
+</div>
+<div id="footer" class="K10">
+<br />
+<br />
+</div></body></html>
---
+++ wp-content/themes/textback/sidebar.php
@@ -0,0 +1,34 @@
+<div id="side">
+<div id="sidebar">
+		
+		
+		<ul>
+					
+
+<br/>
+		<p>
+		Book an appointment online with Celia now.  Brianna is not able to accept new clients at this time. Thank you!
+		</p>
+	
+<br/>
+<br/>	  
+<a href="http://wallflowerssalon.salonmonster.com" target="_blank"><img src="http://www.salonmonster.com/images/buttons/book-now-white-clock-wide.png" /></a>
+
+<br/>
+<br/>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<div class="fb-like" data-href="http://www.facebook.com/pages/Wallflowers-Salon/103836686350664" data-send="true" data-width="450" data-show-faces="false" data-font="arial"></div>
+
+		</ul>
+			<p>
+			Now accepting credit cards.
+			</p>
+			</div>
